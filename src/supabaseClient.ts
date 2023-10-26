7c0a5f505a194c707f1d22f0e397c7104dbca3e4
hgyeom <rrune3@naver.com>
2023-10-26 15:03:00 +0900

    supabaseRealtimeTest

---
+++ src/supabaseClient.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl: string = process.env.REACT_APP_SUPABASE_URL || '';
+const supabaseAnonKey: string = process.env.REACT_APP_ANON_KEY || '';
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export default supabase;
